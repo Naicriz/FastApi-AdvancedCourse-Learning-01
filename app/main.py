d2c7c2e36f7e0999da763b44ad526291acd338cf
isalazarjara@outlook.com <Ian Salazar>
2022-08-09 01:27:35 -0400

    summary and details - almost

--- app/main.py
+++ app/main.py
@@ -13,7 +13,10 @@ from fastapi import Body, Query, Path, Form, Header, Cookie, File, UploadFile
 
 app = FastAPI()
 
+
+    # ------------------------- #
     ## Models for the API endpoints (pydantic) ##
+    # ------------------------- #
 
 class HairColor(Enum): 
     white = "white"
@@ -74,14 +77,27 @@ class LoginOut(BaseModel): # Solo retornará el username ###y el token?)
     username: str = Field(..., min_length=1, max_length=20, example="JhonDoe98")
     message: str = 'Succesfully Logged'
 
+
     ##Path Parameters are always required!##
+    # --------------------------------- #
     # Request and Response Body (FastAPI)
+    # --------------------------------- #
 
 @app.get(
     path = "/",
-    status_code = 200
+    status_code = 200,
+    tags = ["Home"],
+    summary = "Home Page"
     )
-def home(): 
+def home():
+    """
+    ## Home page ##
+
+    Index page of the API.
+
+    Returns:
+        Returns a dictionary with the key "Hello" and the value "World"
+    """
     return {"Hello": "World"}
 
 """
@@ -98,12 +114,32 @@ def create_person(person: Person = Body(...)):
     path = "/person/new",
     response_model = PersonOut,
     status_code = 201, # status 201 significa que se creó un nuevo recurso
-    tags = ["Persons"] # tags are used to group endpoints in the documentation.
+    tags = ["Persons"], # tags are used to group endpoints in the documentation.
+    summary = "Creates a new person in the system"
     )
-def create_person(person: Person = Body(...)): 
+def create_person(
+    person: Person = Body(...)
+    ):
+    """
+        ## Create new Person ##
+        
+        This endpoint*(path operation)* will create a new person based on the data in the body of the request
+        and so, it creates a new person in the application and saves the information in the database.
+        
+        Parameters:
+        - Request body parameters:
+            - **person: Person** => A *person model* that contains *first_name*, *last_name*, *age*, *hair_color* *is_married* and *password*
+
+        Returns:
+        - A newly created person withouth the password.
+        - **person: PersonOut** => A *person model* that contains *first_name*, *last_name*, *age*, *hair_color* and *is_married*
+    """
     return person
 
+
+    # --------------------------------- #
     # Validaciones: Query Parameters (FastAPI)  [Query Parameters are always optional]
+    # --------------------------------- #
 
 @app.get(
     path = "/person/detail",
@@ -129,7 +165,10 @@ def show_person(
 ): 
     return {name: age}
 
+
+    # --------------------------------- #
     # Validaciones: Path Parameters (FastAPI)
+    # --------------------------------- #
 
 persons = [1, 2, 3, 4, 5] # Lista de personas
 
@@ -137,7 +176,8 @@ persons = [1, 2, 3, 4, 5] # Lista de personas
     path = "/person/detail/{person_id}",
     response_model = PersonOut,
     status_code = 200,
-    tags = ["Persons"]
+    tags = ["Persons"],
+    summary = "Shows a person detail"
     )
 def show_person(
     person_id: int = Path(
@@ -147,15 +187,20 @@ def show_person(
         )
 ):  # Si se ingresa un id que no existe, retornará una http exception con status code 404 (Not Found) y el mensaje de error.
     if person_id not in persons:
-        raise HTTPException(status_code=404, detail="Person not found... :(") # raise es una función que lanza una excepción.
+        raise HTTPException(status_code=404, detail="Person not found... :[") # raise es una función que lanza una excepción.
     return {person_id: "It exists!"}
 
+
+    # --------------------------------- #
     # Validaciones: Request Body (FastAPI)
+    # --------------------------------- #
 
 @app.put(
     path = "/person/{person_id}",
+    response_model=PersonOut,
     status_code = 202, # status code 202 significa que se actualizó un recurso
-    tags = ["Persons"]
+    tags = ["Persons"],
+    summary="Updates a person in the system"
     )
 def update_person(
     person_id: int = Path(
@@ -173,26 +218,48 @@ def update_person(
     #return results
     return person
 
-    # Forms
+
+    # --------------------------------- #
+    #     --------- Forms ---------     #
+    # --------------------------------- #
 
 @app.post(
     path = "/login",
     response_model = LoginOut,
     status_code = 200,
-    tags = ["Persons", "Login"]
+    tags = ["Login"],
+    summary = "Log in to the system"
 )
 def login( #campos de formulario que vendrán del frontend.
     username: str = Form(...),
     password: str = Form(...)
     ):
+    """
+        ## Login ##
+        
+        It will login the user and return the username and the token.
+
+        Parameters:
+        - Request body parameters:
+            - **username: str** => The username of the user.
+            - **password: str** => The password of the user.
+        
+        Returns:
+        - LoginOut(username = username) => A *login model* that contains *username*, message and *(soon)* a *token*.
+        It has the variables on it to return the data as a dictionary.
+    """
     return LoginOut(username = username) # la clase a retornar es LoginOut y esta al crear el objeto lo convierte a JSON.
 
+
+    # --------------------------------- #
     # Parametros: Cookies and Headers (FastAPI)
+    # --------------------------------- #
 
 @app.post(
     path = "/contact",
     status_code=200,
-    tags = ["Forms"]
+    tags = ["Forms"],
+    summary = "Creates a new contact"
 )
 def contact(
     first_name: str = Form(
@@ -222,22 +289,38 @@ def contact(
 ):
     return user_agent
 
+
+    # --------------------------------- #
     # Parametros: Files (FastAPI)
+    # --------------------------------- #
 
 @app.post(
     path = "/upload-image",
     status_code = 200,
-    tags = ["Files"]
+    tags = ["Files"],
+    summary = "Uploads an image"
 )
 def post_image(
     image: UploadFile = File(...)
 ):
-    return {
-        "File Name": image.filename, # nombre del archivo
-        "Content Type": image.content_type, # content_type es el tipo de archivo que se subió
-        #"Size (kb)": round(len(image.file.read())/1024, ndigits = 2) # len(image.file.read()) es el tamaño en bytes y se divide por 1024 para obtener el tamaño en kilobytes.
-        "Size (mb)": round(len(image.file.read())/(1024*1024), ndigits = 2) # len(image.file.read()) es el tamaño en bytes y se divide en 1024*1024 para obtener el tamaño en megabytes.
+    """
+        ## Post Image
 
+        This endpoint will receive an image and save it in the server.
+        
+        Parameters:
+        - Request body parameters:
+            - **image: UploadFile** => A *File* that contains the image
+
+        Returns:
+        - dict: A dictionary with the image's information. The image's name, content type and the image's size
+
+    """
+    return {
+        "File name": image.filename, # nombre del archivo
+        "Content type": image.content_type, # content_type es el tipo de archivo que se subió
+        "Size (mb)": round(len(image.file.read())/(1024*1024), ndigits = 2) # len(image.file.read()) es el tamaño en bytes y se divide en 1024*1024 para obtener el tamaño en megabytes
+        #"Size (kb)": round(len(image.file.read())/1024, ndigits = 2) # len(image.file.read()) es el tamaño en bytes y se divide por 1024 para obtener el tamaño en kilobytes
     }
 
 """
